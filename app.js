d3f53c8b9612e0b9a0a9fade755ded900d5e58cd
Matthieu <reungoat.matthieu@gmail.com>
2023-02-07 09:34:26 +0100

    add config.env file

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // 1) MIDDLEWARE
-app.use(morgan('dev'));
+process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware 💥💥💥');
   next();
